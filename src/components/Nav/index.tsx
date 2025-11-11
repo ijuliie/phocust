09aba3d44273eaebe46720827e72c89e1718c729
Julie <ijuliie@aim.com>
2025-11-11 16:57:29 -0700

    add home link

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -7,6 +7,11 @@ export default function Nav() {
         <Container>
             <div className={`${styles.nav}`}>
                 <div>
+                    <Link href="/">
+                        Home
+                    </Link>
+                </div>
+                <div>
                     <Link href="/music">
                         Music
                     </Link>
