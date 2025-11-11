6a3af894498cb780285a2d7c98d07f7b2896367d
Julie <ijuliie@aim.com>
2025-11-11 16:54:34 -0700

    update

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <Fade>
             <div className={`${styles.imageContainer}`}>
-                <Image src="/SLC_01.jpg" alt="image of phocust" />
+                <Image height={600} width={600} src="/SLC_01.jpg" alt="image of phocust" />
                 <div className={`${styles.imgOverlay}`}>
                     <Container>
                         <img src="/white with line.png" alt="" />
