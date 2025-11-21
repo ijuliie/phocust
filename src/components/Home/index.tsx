0c2bbd3321f3b323d7cd8b11f2fc251de0b0c1fd
Julie <ijuliie@aim.com>
2025-11-21 15:29:06 -0700

    fix photo

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <Fade>
             <div className={`${styles.imageContainer}`}>
-                <img src="/SLC_01.jpg" alt="image of phocust" />
+                <img src="/SLC_02.jpg" alt="image of phocust" />
                 <div className={`${styles.imgOverlay}`}>
                     <Container>
                         <img src="/white with line.png" alt="" />
